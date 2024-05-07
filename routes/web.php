9f8db32b400e08b8ef3cf4437e2b525dff2d8da9
Fajar Aji Prayoga <fajaraji.prayoga@gmail.com>
2024-05-07 11:32:30 +0700

    Test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function() {
+    $response = Http::acceptJson()->get("https://stamping.newarmada.co.id/api/about");
+
+    return $response->json();
 });
