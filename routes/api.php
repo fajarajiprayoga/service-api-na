ee83153c6be4aa86cefad2ad71d7cb9cd3c5cd62
Fajar Aji Prayoga <fajaraji.prayoga@gmail.com>
2024-05-07 11:15:29 +0700

    Test Fetch API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function() {
+    $response = Http::acceptJson()->get("https://stamping.newarmada.co.id/api/about");
+
+    return $response->json();
 });
